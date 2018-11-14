2a1697db548501ab31591b6f5c1545b31a7a690d
Alan <al.zhang93@gmail.com>
2018-11-14 09:46:39 -0800

    finished implementing promises for api route functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
     "extends": "airbnb",
     "rules": {
         "func-names": ["error", "never"],
-        "camelcase": [2, {properties: "never"}],
+        "camelcase": ["error", {properties: "never"}],
     }
 };
