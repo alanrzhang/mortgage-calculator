6e5b650af3ffdb20e3ad19c0d8141126417a8026
Alan <al.zhang93@gmail.com>
2018-11-16 17:27:06 -0800

    added bash file to seed data into mongo. refactored data generation file for time efficiency

--- database/fake.js
+++ database/fake.js
@@ -1,33 +1,30 @@
 const faker = require('faker');
 const moment = require('moment');
-const json2csv = require('json2csv').parse;
 const fs = require('fs');
-require('dotenv').config();
+
+const getRandomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min));
+};
 
 const streetNames = new Set();
 faker.seed(123);
 while (streetNames.size < 1000) {
   streetNames.add(faker.address.streetName());
 }
+
 const streetArray = Array.from(streetNames);
+
 const seedData = function (streets) {
+  fs.writeFileSync('./database/data.csv', 'id,address,home_price,property_tax,home_insurance,hoa_dues');
   const startTime = moment().valueOf() / 1000;
   streets.forEach((val, index) => {
     console.log(`generating data for street name: ${index + 1}`);
-    const data = [];
+    let csv = '';
     for (let i = 1; i <= 10000; i += 1) {
-      const dataObject = {
-        id: i + (10000 * index),
-        address: i + ' ' + val,
-        home_price: faker.random.number({ min: 200000, max: 1500000 }),
-        property_tax: faker.random.number({ min: 1000, max: 5000 }),
-        home_insurance: faker.random.number({ min: 400, max: 1000 }),
-        hoa_dues: faker.random.number({ min: 100, max: 500 }),
-      };
-      data.push(dataObject);
+      const stringEntry = `\n${i + (10000 * index)},${i + ' ' + val}, ${getRandomNumber(200000, 1500000)}, ${getRandomNumber(1000, 5000)},${getRandomNumber(400, 1000)},${getRandomNumber(100, 500)}`;
+      csv += stringEntry;
     }
-    const csv = json2csv(data, { header: false });
-    fs.appendFileSync('./database/data.csv', '\n' + csv);
+    fs.appendFileSync('./database/data.csv', csv);
   });
   const endTime = moment().valueOf() / 1000;
   console.log(`Data generation took ${endTime - startTime} seconds`);
